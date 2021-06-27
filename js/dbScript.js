de14efe32fa2fdb8c270f093f79b3b4051d07c11
Kushagra Garg <kushagragarg1291@gmail.com>
2021-06-27 19:02:04 +0530

    save, delete recording and ui additions

--- js/dbScript.js
+++ js/dbScript.js
@@ -1,6 +1,7 @@
 let dbAccess;
 let request = indexedDB.open("Karaoke", 1);
-let container = document.querySelector(".container")
+let tbody = document.querySelector("tbody");
+let body = document.querySelector("body");
 
 request.addEventListener("success", function () {
     dbAccess = request.result;
@@ -8,7 +9,7 @@ request.addEventListener("success", function () {
 
 request.addEventListener("upgradeneeded", function () {
     let db = request.result;
-    db.createObjectStore("recordings", { keyPath: "mId" });
+    db.createObjectStore("recordings", { keyPath: "rId" });
 });
 
 request.addEventListener("error", function () {
@@ -18,8 +19,18 @@ request.addEventListener("error", function () {
 function saveRecording(media, song, startTime, endTime) {
     let tx = dbAccess.transaction("recordings", "readwrite");
     let recordingsObjectStore = tx.objectStore("recordings");
+
+    let totalRecordings = localStorage.getItem("totalRecordings");
+    if (totalRecordings == undefined) {
+        totalRecordings = 0;
+    }
+    totalRecordings = parseInt(totalRecordings);
+    let title = "recording " + (totalRecordings + 1);
+    localStorage.setItem("totalRecordings", totalRecordings + 1);
+
     let data = {
-        mId: Date.now(),
+        rId: Date.now(),
+        title,
         media,
         date: new Date().toLocaleDateString(),
         time: new Date().toLocaleTimeString(),
@@ -30,54 +41,96 @@ function saveRecording(media, song, startTime, endTime) {
     recordingsObjectStore.add(data);
 }
 
-
 function viewMedia() {
     let tx = dbAccess.transaction("recordings", "readonly");
     let recordingsObjectStore = tx.objectStore("recordings");
     let req = recordingsObjectStore.openCursor();
 
+    let counter = 0;
+    tbody.innerHTML = "";
     req.addEventListener("success", function () {
         let cursor = req.result;
 
-        let counter = 1;
         if (cursor) {
+            counter += 1;
             let tr = document.createElement("tr");
             tr.innerHTML = `<td>${counter}</td>
-            <td>002 Bulleya</td>
-            <td>25th June, 2021</td>
+            <td>${cursor.value.title}</td>
+            <td>${cursor.value.date} ${cursor.value.time}</td>
             <td>
-                <button type="button" data-id = "${cursor.value.mId} class="btn btn-success">
+                <button type="button" data-id="${cursor.value.rId}" class="btn btn-success recording-listen-btn" data-bs-toggle="modal"
+                data-bs-target="#staticBackdrop">
                     Listen
                 </button>
-                <button type="button" data-id = "${cursor.value.mId} class="btn btn-danger">
+                <button type="button" data-id="${cursor.value.rId}" class="btn btn-danger recording-delete-btn">
                     Delete
                 </button>
+                <div data-id="${cursor.value.rId}" class="recording-configure-btn">
+                    <img src="https://img.icons8.com/material-outlined/32/000000/settings--v1.png"/>
+                </div>
             </td>`;
-            counter += 1;
 
-            let listenBtn = $("#listen")[0];
-            let deleteBtn = $("#delete")[0];
-
-            deleteBtn.addEventListener("click", function (e) {
-                let mId = e.currentTarget.getAttribute("data-id");
-                e.currentTarget.parentElement.parentElement.remove();
-
-                deleteMediaFromDB(mId);
-            })
-            
-            listenBtn.addEventListener('click', function(e) {
-
-            })
-
-            container.appendChild(tr);
+            tbody.appendChild(tr);
             cursor.continue();
         }
+
+        else if (counter == 0) {
+            let tr = document.createElement("tr");
+            tr.innerHTML = `<td></td>
+            <td>No Recordings available!</td>
+            <td></td>
+            <td></td>`;
+
+            tbody.append(tr);
+        }
+
+        let listenBtn = document.querySelector(".recording-listen-btn");
+        let deleteBtn = document.querySelector(".recording-delete-btn");
+
+        deleteBtn.addEventListener("click", function (e) {
+            let rId = e.currentTarget.getAttribute("data-id");
+            e.currentTarget.parentElement.parentElement.remove();
+
+            deleteMediaFromDB(rId);
+        })
+
+        listenBtn.addEventListener('click', function (e) {
+            let modal = $(`
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="staticBackdropLabel">${e.currentTarget.parentElement.parentElement.children[1].innerText}.mp3</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <audio controls style="width:100%">
+                                    <source src="https://www.w3schools.com/TagS/horse.mp3" type="audio/mpeg">
+                                    Your browser does not support the audio element.
+                                </audio>
+                            </div>
+                            <div class="modal-footer">
+                                <button id="close-btn" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <!-- <button type="button" class="btn btn-primary">Understood</button> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>`);
+            body.appendChild(modal[0]);
+
+            let closeBtn = $("#close-modal-btn");
+            closeBtn.click(function () {
+                body.removeChild(modal[0]);
+            })
+        })
     })
 }
 
-
-function deleteMediaFromDB(mId) {
+function deleteMediaFromDB(rId) {
     let tx = dbAccess.transaction("recordings", "readwrite");
     let recordingsObjectStore = tx.objectStore("recordings");
-    recordingsObjectStore.delete(Number(mId))
+    recordingsObjectStore.delete(Number(rId))
+    localStorage.setItem("totalRecordings", localStorage.getItem("totalRecordings") - 1);
+    viewMedia();
 }
--- js/script.js
+++ js/script.js
@@ -2,19 +2,17 @@
 let currentSong = "";
 let isRecording = false;
 let startBtn = $('#start-record')[0];
-let pauseBtn = $('#pause-record')[0];
+// let pauseBtn = $('#pause-record')[0];
 let stopBtn = $('#stop-record')[0];
 let ytPlayer = $("#video-player")[0];
+let startRecordingTime, endRecordingTime;
+let playPushed = false;
 
-// event listener on each song card to change the song
-$(".song-card").click(function (e) {
-    $(".song-card.selected").removeClass("selected");
-    $(e.currentTarget).addClass("selected");
-    let nextSong = e.currentTarget.children[1].innerText;
-    changeSongTo(songs[nextSong]);
-    changeLyrics(nextSong);
-    startBtn.disabled = false;
-})
+// event listener on start btn
+startBtn.addEventListener("click", function () {
+    alert("Click on any song card to start recording");
+});
+
 
 // event listener to switch the navbar tabs
 $(".nav-link").click(function (e) {
@@ -30,36 +28,54 @@ $(".nav-link").click(function (e) {
     } else {
         $(".recordings-container").removeClass("hidden");
         $(".karaoke-container").addClass("hidden");
+        viewMedia();
     }
 });
 
-$(".song-search-input").click(function (e) {
-    $(".nav-link.active").removeClass("selected");
-    $("#custom-song-card").addClass("selected");
+
+// event listener listent on custom play
+$(".custom-play").click(function (e) {
+    let customVideoId = e.currentTarget.parentElement.children[0].value.split("=")[1];
+    console.log(customVideoId);
+    if (customVideoId) {
+        $(".nav-link.selected").removeClass("selected");
+        $("#custom-song-card").addClass("selected");
+        changeSongTo(customVideoId);
+        changeLyrics("Custom Song");
+        playPushed = true;
+        // $("#custom-song-card").click();
+    } else {
+        alert("Youtube link Invalid!");
+    }
 })
+
 
 // function to change the song
 function changeSongTo(songId) {
     $("#video-player").attr("src", "https://www.youtube.com/embed/" + songId + "?autoplay=1&controls=0");
 }
 
+
 // function to change the lyrics on the right panel
 function changeLyrics(nextSong) {
-    currentSong = nextSong;
-    $(".lyrics-header h1:nth-child(1)").text(currentSong);
     $(".lyrics-text").empty();
-    let currentLyrics = lyricsData[currentSong];
+    let currentLyrics = lyricsData[nextSong];
 
     if (currentLyrics == undefined) {
         let p_tag = `<p>Lyrics not available</p>`;
         $(".lyrics-text").append(p_tag);
+        currentSong = "Custom";
     } else {
+        currentSong = nextSong;
         currentLyrics.forEach(element => {
             let p_tag = `<p>${element}</p>`;
             $(".lyrics-text").append(p_tag);
         });
     }
+
+    $(".lyrics-header h1:nth-child(1)").text(currentSong);
 }
+
 
 // function to download file
 function downloadFile(link) {
@@ -71,43 +87,67 @@ function downloadFile(link) {
 }
 
 
-
 navigator.mediaDevices.getUserMedia({ audio: true })
 
     .then(function (userStream) {
-        let audio = document.createElement("audio");
-        audio.autoplay = true;
-
         let mediaRecorder = new MediaRecorder(userStream);
 
         let chunks = [];
 
-        startBtn.addEventListener('click', function (e) {
-            $(".container").append(audio);
+        // event listener on each song card to change the song
+        $(".song-card").click(function (e) {
+            if (isRecording == true) {
+                stopBtn.click();
+            }
+            if (e.currentTarget.innerText == "Custom" && playPushed == false) {
+                alert("Error! Enter youtube link and push play to use this card");
+                return;
+            }
+            else if (e.currentTarget == "Custom") {
+                playPushed = false;
+            }
+            else {
+                $(".song-card.selected").removeClass("selected");
+                $(e.currentTarget).addClass("selected");
+                let nextSong = e.currentTarget.children[1].innerText;
+                changeSongTo(songs[nextSong]);
+                changeLyrics(nextSong);
+            }
+            
             startBtn.hidden = true;
-            pauseBtn.hidden = false;
+            // pauseBtn.hidden = false;
             stopBtn.hidden = false;
             mediaRecorder.start();
             isRecording = true;
+            startRecordingTime = new Date().getSeconds();
+            console.log("recording started");
+            console.log(startRecordingTime);
+            
         })
 
-        pauseBtn.addEventListener('click', function (e) {
-            if (isRecording) {
-                pauseBtn.innerText = "Resume Recording"
-                mediaRecorder.pause();
-                isRecording = false;
-            } else {
-                pauseBtn.innerText = "Pause Recording"
-                mediaRecorder.resume();
-                isRecording = true;
-            }
-        })
+        // pauseBtn.addEventListener('click', function (e) {
+        //     if (isRecording) {
+        //         pauseBtn.innerText = "Resume Recording"
+        //         mediaRecorder.pause();
+        //         isRecording = false;
+        //     } else {
+        //         pauseBtn.innerText = "Pause Recording"
+        //         mediaRecorder.resume();
+        //         isRecording = true;
+        //     }
+        // })
 
         stopBtn.addEventListener('click', function (e) {
             mediaRecorder.stop();
+            isRecording = false;
             startBtn.hidden = false;
-            pauseBtn.hidden = true;
+            // pauseBtn.hidden = true;
             stopBtn.hidden = true;
+            endRecordingTime = new Date().getSeconds();
+            console.log("recording stopped");
+            console.log(endRecordingTime);
+            $("#video-player").attr("src", "https://www.youtube.com/embed/z4c14QYnBoU?autoplay=1&mute=1&controls=0");
+            $(".song-card.selected").removeClass("selected");
         });
 
         mediaRecorder.ondataavailable = function (e) {
@@ -117,10 +157,14 @@ navigator.mediaDevices.getUserMedia({ audio: true })
         mediaRecorder.onstop = function (e) {
             let audioData = new Blob(chunks, { 'type': 'audio/mp3;' });
             chunks = [];
-
             let audioSrc = window.URL.createObjectURL(audioData);
-
-            downloadFile(audioSrc);
+            let diff = endRecordingTime - startRecordingTime;
+            let minutes = parseInt(diff / 60);
+            let seconds = diff % 60;
+            console.log(`${minutes}:${seconds}`);
+            saveRecording(audioSrc, currentSong, "00:00", `${minutes}:${seconds}`);
+            alert("Recording saved in My Recordings");
+            // downloadFile(audioSrc);
         }
     })
 
